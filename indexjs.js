7aac9d9356152bfeed981661c404ff7dc3489bc6
Faritha Nasreen <faritha@freshdesk.com>
2016-06-28 14:35:27 +0530

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,80 +1,100 @@
-var turn = 0;//for player 1 its 0 and for player 2 its 1
-var arrX = [0,0,0,0,0,0,0,0,0];
-var arrO = [0,0,0,0,0,0,0,0,0];
-var count = 0;
+var socket = io.connect('http://localhost:3000');
+var height = 0;
+// on connection to server, ask for user's name with an anonymous callback
+socket.on('connect', function(){
+    // call the server-side function 'adduser' and send one parameter (value of prompt)
+    socket.emit('adduser', prompt("What's your name?"));
+});
+
+// listener, whenever the server emits 'updatechat', this updates the chat body
+socket.on('updatechat', function (username, data) {
+    $('#conversation').append('<p><b>' + username + ':</b> ' + data + '</p>');
+    $('#conversation').animate({scrollTop: height});
+});
+
+// listener, whenever the server emits 'updaterooms', this updates the room the client is in
+socket.on('updaterooms', function(rooms, current_room) {
+    alert(current_room.id);
+    $('#rooms').empty();
+    $.each(rooms, function(key, value) {
+           //alert(value.id);
+           //alert(current_room.id); 
+        if(value.id == current_room.id){
+            $('#rooms').append('<div><a href="#" class="selected">' + value.id + '</a></div>');
+        } 
+        else{
+            $('#rooms').append('<div><a href="#" onclick="switchRoom(\''+ value.id +'\')">' + value.id + '</a></div>');
+        }
+    });
+});
+
+socket.on('updategame', function (id, v) {
+    $('#'+id).text(v);
+});
+
+
+socket.on('onWin', function (who){
+    window.alert(who);
+});
+
+socket.on('onTie', function (res){
+    window.alert(res);
+});
+
+socket.on('restartgame', function(){
+    for ( var i = 0; i < 9; i++){
+        $('#'+i).text('');
+    }
+});
+
+socket.on('refreshGame', function(room){
+    for ( var i = 0; i < 9; i++){
+        $('#'+i).text('');
+    }
+    for ( var i = 0; i < 9; i++){
+        if(room.arrX[i] != '')
+        $('#'+i).text('x');
+        if(room.arrO[i] != '')
+        $('#'+i).text('o');
+    }
+});
+
+$(function(){
+    $('#datasend').click( function() {
+        
+        var message = $('#data').val();
+        $('#data').val('');
+        socket.emit('sendchat', message );
+    });
+
+    $('#data').keypress(function(e) {
+        if(e.which == 13) {
+            $(this).blur();
+            $('#datasend').focus().click();
+        }
+    });
+    $('#conversation').each(function(i, value){
+        height += parseInt($(this).height());
+    });
+    height += '';
+
+});
 
 function change(id){
-if(turn == 0)
-{
-		document.getElementById(id).innerHTML = 'x';
-    count += 1;
-    arrX[id] = Math.pow(2,id);
-		checkWin(arrX,'X wins');
-    checkDraw();
-    turn = 1;
-}
-else{
- 		document.getElementById(id).innerHTML = 'o';
-    count += 1;
-    arrO[id] = Math.pow(2,id);
-		checkWin(arrO, 'O wins');
-    checkDraw();
-    turn = 0;
-} 
+    socket.emit('changeValue', id);
 }
 
-function setAll(a, v) {
-    var i, n = a.length;
-    for (i = 0; i < n; ++i) {
-        a[i] = v;
-    }
-}
-function newGame()
-{
-		document.getElementById("0").innerHTML = '';
-    document.getElementById("1").innerHTML = '';
-    document.getElementById("2").innerHTML = '';
-    document.getElementById("3").innerHTML = '';
-    document.getElementById("4").innerHTML = '';
-    document.getElementById("5").innerHTML = '';
-    document.getElementById("6").innerHTML = '';
-    document.getElementById("7").innerHTML = '';
-    document.getElementById("8").innerHTML = '';
-    turn = 0;
-		setAll(arrX,0);
-    setAll(arrO,0);
-    count = 0;
+function startnew(){
+    socket.emit('startover');
 }
 
-function checkWin(arr,str)
-{
-		var winCombinations = [7,56,448,73,146,292,273,84];
-    if((arr[0] + arr[1] + arr[2] == 7) || (arr[3] + arr[4] + arr[5] == 56) ||
-    (arr[6] + arr[7] + arr[8] == 448) || (arr[0] + arr[3] + arr[6] == 73) ||
-    (arr[1] + arr[4] + arr[7] == 146) || (arr[2] + arr[5] + arr[8] == 292) ||
-    (arr[0] + arr[4] + arr[8] == 273) || (arr[2] + arr[4] + arr[6] == 84))
-    {
-    		alert (str);
-        newGame();
-    }
+function createRoom(){
+    socket.emit('createNewRoom');
 }
 
-function checkDraw()
-{
-		if (count == 9)
-    {
-    		window.alert ("It's a tie!!");
-        newGame();
-    }
+function switchRoom(roomid){
+    socket.emit('switchRoom',roomid);
 }
-
-
-
-
-
-
-
-
 
 
 
